e969807b353c97b8084046dbeb4792ee2e7df6e3
HA-TTP-Work <76753576+HA-TTP-Work@users.noreply.github.com>
2021-01-07 12:01:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,22 +7,40 @@ document.getElementById("container");
 
 //2) Select the section with an id of container using querySelector.
 
+document.querySelector("container");
+
 
 
 //3) Select all of the list items with a class of "second".
 
+document.getElemenstbyClassName("second")
+
 
 //4) Select a list item with a class of third, but only the list item inside of the ol tag.
+
+document.getElementsByTagName("ol").getElemenstbyClassName("third")
+
+// maybe break up into 2 lines?
 
 
 
 //5) Give the section with an id of container the text "Hello!".
 
 
+document.getElementById("container").innerHTML = "Hello!";
+// any tags?
+
+
 //6) Add the class main to the div with a class of footer.
+
+document.getElementByClassName("fotter").innerHTML = <div class="main"></div>;
+
+
 
 
 //7) Remove the class main on the div with a class of footer.
+
+
 
 
 //8) Create a new li element.
